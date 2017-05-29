455f871484a2bb47a9550a9d485beef8be2ac1d8
Dair <dairmakeshov@gmail.com>
2017-05-29 20:38:05 +0600

    Frontend'

--- app/models/bazar.rb
+++ app/models/bazar.rb
@@ -2,7 +2,7 @@ class Bazar < ApplicationRecord
 	has_many :podrazdels
 	
 	has_attached_file :image,
-        styles: { medium: '300x300>', thumb: '300x230>'},
+        styles: { medium: '300x300>', thumb: '256x256>'},
         default_url: '/img/productav.png'
  	validates_attachment_content_type :image, 
        	content_type: ['image/jpeg', 'image/gif', 'image/png']
